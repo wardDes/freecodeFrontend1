e27d192ecaada8b1dc9c02ac000b5e726e301360
Daniel Howard <danielh9049@att.net>
2019-09-03 18:49:58 -0700

    add div container with id of quote-box to to fulfill project requirement

--- quotemachine/src/components/DisplayQuoteAuthors.js
+++ quotemachine/src/components/DisplayQuoteAuthors.js
@@ -11,7 +11,7 @@ export class DisplayQuoteAuthors extends React.Component {
     }
 
     componentDidMount(){
-        
+
     }
 
     render(){
@@ -19,24 +19,26 @@ export class DisplayQuoteAuthors extends React.Component {
             <div>
                 <div>
                     <div className="wrapper"> 
-                      <div className="quote-text">
-                          <i className="fa fa-quote-left"> </i><span id="text"></span>
-                      </div>
-                      <div className="quote-author">
-                          - <span id="author"></span>
-                      </div>
-                      <div className="buttons">
-                          <div id="bstart">
-                              <a className="button" href="https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=somequote" id="tweet-quote" title="Tweet this quote!" target="_blank" rel="noopener noreferrer">
-                                  <i className="fa fa-twitter"></i>
-                              </a>
-                              <a className="button" href="https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=currentAuthor&content=currentQuote&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button" id="tumblr-quote" title="Post this quote on tumblr!" target="_blank" rel="noopener noreferrer">
-                                  <i className="fa fa-tumblr"></i>
-                              </a>
-                          </div>
-                          <div id="bend">
-                              <button className="button" id="new-quote">New quote</button>
-                          </div>
+                      <div id="quote-box">
+                        <div className="quote-text">
+                            <i className="fa fa-quote-left"> </i><span id="text"></span>
+                        </div>
+                        <div className="quote-author">
+                            - <span id="author"></span>
+                        </div>
+                        <div className="buttons">
+                            <div id="bstart">
+                                <a className="button" href="https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=somequote" id="tweet-quote" title="Tweet this quote!" target="_blank" rel="noopener noreferrer">
+                                    <i className="fa fa-twitter"></i>
+                                </a>
+                                <a className="button" href="https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=currentAuthor&content=currentQuote&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button" id="tumblr-quote" title="Post this quote on tumblr!" target="_blank" rel="noopener noreferrer">
+                                    <i className="fa fa-tumblr"></i>
+                                </a>
+                            </div>
+                            <div id="bend">
+                                <button className="button" id="new-quote">New quote</button>
+                            </div>
+                        </div>
                       </div>
                   </div>
               </div>
